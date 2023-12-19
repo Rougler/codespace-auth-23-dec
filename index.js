ccd841da629967295e61ef4ad685f1f24a108638
Rudraprasad Mohapatra <98683883+Rudraprasad-Mohapatra@users.noreply.github.com>
2023-12-19 11:23:26 +0000

    First Home route created.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("Hello to codespace");
-console.log(process.env.PORT)
+require('dotenv').config();
+const {PORT} = process.env;
+
+const app = require("./app")
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working.</h1>")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port: ${PORT}`)
+})
